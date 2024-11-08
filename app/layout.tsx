f6cd263347d7f9a768e9a5dc4097dc15ac02ad97
thorski1 <sthoyre@gmail.com>
2024-11-08 13:32:12 -0500

    [main] feat: add more to the tile

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>LangChain + Next.js Template</title>
+        <title>LangChain + Next.js Template yayyyy</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta
           name="description"
